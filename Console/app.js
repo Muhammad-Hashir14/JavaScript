2ebfc318cefa7928b2869950e2b8183709e73692
Muhammad Hashir <hashirgohar@gmail.com>
2023-03-21 21:58:22 +0500

    Added console file in javasript folder

---
+++ Console/app.js
@@ -0,0 +1,8 @@
+// Alert
+alert('Hello world');
+// document write
+document.write("Hello World");
+// Console
+console.log("Hello World");
+console.warn("This is an warning")
+console.error("This is an error")
