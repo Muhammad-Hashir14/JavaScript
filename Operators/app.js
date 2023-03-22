213a86329099df1b904ac5032d83d939fc5c854a
Muhammad Hashir <hashirgohar@gmail.com>
2023-03-23 00:55:35 +0500

    Added operators file in javasript folder

---
+++ Operators/app.js
@@ -0,0 +1,30 @@
+console.log("Operators in JavaScript");
+document.write("Operators in JavaScript <br>");
+document.write("there are three types of operators<br>");
+document.write("Arithmetic operatos<br>");
+document.write("Comparision operators<br>");
+document.write("Logical operators");
+
+var num1 = 122;
+var num2 = 34;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+
+// comparision operators
+console.log(num1 == num2);
+console.log(num1 < num2);
+console.log(num1 > num2);
+
+// logical operators
+console.log(true && true);
+console.log(false && true);
+
+console.log(true || true);
+console.log(false || true);
+
+
+console.log(!true);
+console.log(!false);
