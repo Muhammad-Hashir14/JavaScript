090559e4ec9d39a7e96ce05352a69603dd7a41e6
Muhammad Hashir <hashirgohar@gmail.com>
2023-03-23 00:28:18 +0500

    Added datatypes files

---
+++ Datatypes/app.js
@@ -0,0 +1,33 @@
+console.log("Variables");
+document.write("Variables practice");
+
+// variables
+var num1 = 2;
+var num2 = 4;
+console.log(num1 + num2);
+
+var str1 = "Hello";
+var str2 = "World";
+console.log(str1 + " " + str2);
+
+var flt = 3.12;
+var flt2 = 4.12;
+console.log(flt + flt2);
+
+var bool1 = true;
+var bool2  = false;
+console.log(bool1);
+console.log(bool2);
+
+var obj = {
+    Talha:12,
+    umer: 21,
+    owais:0
+};
+console.log(obj);
+console.log(obj["Talha"]);
+console.log(obj["owais"]);
+
+var arr = ["pakistan",12,43,true,'apple'];
+console.log(arr);
+console.log(arr[3]);
