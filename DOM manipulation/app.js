464649826c6c07e52edd7b23dcf3a765abb15671
Muhammad Hashir <hashirgohar@gmail.com>
2023-03-25 00:21:56 +0500

    Added DOM manupulation file

--- DOM manipulation/app.js
+++ DOM manipulation/app.js
@@ -2,3 +2,14 @@ console.log("DOM Manipulation");
 
 let  e = document.getElementById("em");
 console.log(e);
+let a = document.getElementsByClassName("container");
+console.log(a);
+// let c = document.getElementById("para");
+console.log(c);
+document.getElementById("para").style.color = "white"
+document.getElementById("para").style.backgroundColor = "orange";
+// let ab = document.getElementsByClassName("para");
+// ab.classList.add("change")
+let inht = document.getElementsByClassName("para");
+console.log(inht.innerHTML);
+console.log(inht.innerHTML)
